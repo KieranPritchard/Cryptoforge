16df61639081c11eb1b28774de519ed150871ddf
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-05-02 11:40:12 +0100

    Added rsa file decryption.

--- Untitled-Cryptography-App.py
+++ Untitled-Cryptography-App.py
@@ -340,6 +340,25 @@ class RSA:
         file = open(file, "wb")
         encrypted_file = file.write(encrypted_contents)
 
+    def rsa_file_decryption(private_key,file):
+
+        file = open(file, "rb")
+        encrypted_contents = file.read()
+        file.close()
+
+        decrypted_contents = private_key.decrypt(
+            encrypted_contents,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+
+        file = open(file, "wb")
+        file.write(decrypted_contents)
+        file.close()
+
 class SHA_2:
     def __init__(self):
         pass
