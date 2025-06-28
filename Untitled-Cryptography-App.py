766b02710c6966a632c3483fe2fd3bbbc3c78c7d
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-06-28 11:33:00 +0100

    added signing and verifying files

--- Untitled-Cryptography-App.py
+++ Untitled-Cryptography-App.py
@@ -872,3 +872,25 @@ class RSA_digital_signatures:
             print("Signature is valid.")
         except Exception as e:
             print("Signature is invalid:", e)
+
+    def ecdsa_sign_file(file_path, signature_path, private_key):
+        with open(file_path, "rb") as f:
+            data = f.read()
+
+        signature = private_key.sign(data, ec.ECDSA(hashes.SHA256()))
+
+        with open(signature_path, "wb") as f:
+            f.write(signature)
+
+    def ecdsa_verify_file(file_path, signature_path, public_key):
+        with open(file_path, "rb") as f:
+            data = f.read()
+
+        with open(signature_path, "rb") as f:
+            signature = f.read()
+
+        try:
+            public_key.verify(signature, data, ec.ECDSA(hashes.SHA256()))
+            print("✅ Signature is valid.")
+        except InvalidSignature:
+            print("❌ Signature is INVALID.")
