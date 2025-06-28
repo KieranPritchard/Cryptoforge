e9a8f9faf3b050302805c1aae4fdfa7f245da944
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-06-28 17:35:57 +0100

    added crud arguements

--- Cryptoforge.py
+++ Cryptoforge.py
@@ -12,3 +12,19 @@ import src.sha_200_hash_functions
 import src.sha_300_hash_functions
 
 parser = argparse.ArgumentParser(description="My personal cryptography ultility consult the readme file for more information.")
+parser.add_argument("function")
+# arguments for key management
+# save key arguements
+parser.add_argument("--save-key", type=str, help="function that saves key that is currently loaded to config defined folder.")
+parser.add_argument("--new-key-name", type=str, help="specifies the name of the key that is being used.")
+parser.add_argument("--key-type", type=str, help="specifies type of key to save.")
+# load key arguements
+parser.add_argument("--load-key", type=str, help="function that loads key from config defined folder.")
+# list key arguements
+parser.add_argument("--list-keys", type=str, help="lists keys in the key folder.")
+#rename key arguements
+parser.add_argument("--old-name", type=str, help="old name of the key to rename.")
+parser.add_argument("--new-name", type=str, help="new name of the key to rename.")
+# delete key arguements
+parser.add_argument("--delete-key", type=str, help="specifies key to delete.")
+args = parser.parse_args()
