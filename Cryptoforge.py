fb9213d785de02387b4e671fefa8c761e9238b55
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-06-28 12:17:33 +0100

    imported custom functions

--- Cryptoforge.py
+++ Cryptoforge.py
@@ -1,3 +1,14 @@
 import argparse
+# my custom imports for the functions.
+import src.aes_algorithm_functions
+import src.blake2_hash_algorithm
+import src.blowfish_algorithms_functions
+import src.chacha20_algorithm_fuctions
+import src.ecdsa_digital_signature
+import src.key_management
+import src.ecdsa_digital_signature
+import src.rsa_algorithm_fuctions
+import src.sha_200_hash_functions
+import src.sha_300_hash_functions
 
 parser = argparse.ArgumentParser(description="My personal cryptography ultility consult the readme file for more information.")
