a16102e6f25f36edaa1b79cbd3f650a543db7758
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-07-01 15:18:01 +0100

    broke down handling function further.

--- src/sha_300_hash_functions.py
+++ src/sha_300_hash_functions.py
@@ -107,3 +107,80 @@ class SHA_300:
         file_to_hash.close()
         hash_result_hex = hashlib.sha3_512(data).hexdigest()
         return hash_result_hex
+
+sha300_hash = SHA_300()
+
+def handle_sha300_hash_operations(args):
+    if not args.input or not args.hash_type:
+        print("Hash operations require --input and --hash-type arguments")
+        return
+    
+    # Finds out if the input is a file or text
+    try:
+        with open(args.input, 'rb') as f:
+            data = f.read()
+        is_file = True
+    except:
+        data = args.input.encode()
+        is_file = False
+
+    hash_type = args.hash_type.lower()
+    output_format = args.output_format
+
+    if hash_type.startswith("sha"):
+        if hash_type in ["sha3_224", "sha3_256", "sha3_384", "sha3_512"]:
+            if is_file:
+                if output_format == "hex":
+                    if hash_type == "sha224":
+                        result = sha300_hash.sha3_224_file_hash_hex(args.input)
+                    elif hash_type == "sha256":
+                        result = sha300_hash.sha3_256_file_hash_hex(args.input)
+                    elif hash_type == "sha384":
+                        result = sha300_hash.sha3_384_file_hash_hex(args.input)
+                    elif hash_type == "sha512":
+                        result = sha300_hash.sha3_512_file_hash_hex(args.input)
+                else:
+                    if hash_type == "sha224":
+                        result = sha300_hash.sha3_224_file_hash_bytes(args.input)
+                    elif hash_type == "sha256":
+                        result = sha300_hash.sha3_256_file_hash_bytes(args.input)
+                    elif hash_type == "sha384":
+                        result = sha300_hash.sha3_384_file_hash_bytes(args.input)
+                    elif hash_type == "sha512":
+                        result = sha300_hash.sha3_512_file_hash_bytes(args.input)
+            else:
+                if output_format == "hex":
+                    if hash_type == "sha224":
+                        result = sha300_hash.sha224_plaintext_hash_hex(data)
+                    elif hash_type == "sha256":
+                        result = sha300_hash.sha256_plaintext_hash_hex(data)
+                    elif hash_type == "sha384":
+                        result = sha300_hash.sha384_plaintext_hash_hex(data)
+                    elif hash_type == "sha512":
+                        result = sha300_hash.sha512_plaintext_hash_hex(data)
+                else:
+                    if hash_type == "sha224":
+                        result = sha300_hash.sha224_plaintext_hash_bytes(data)
+                    elif hash_type == "sha256":
+                        result = sha300_hash.sha256_plaintext_hash_bytes(data)
+                    elif hash_type == "sha384":
+                        result = sha300_hash.sha384_plaintext_hash_bytes(data)
+                    elif hash_type == "sha512":
+                        result = sha300_hash.sha512_plaintext_hash_bytes(data)
+        else:
+            print(f"Unsupported hash type: {hash_type}")
+            return
+    
+    # Output result
+    if args.output:
+        with open(args.output, 'w') as f:
+            if output_format == "hex":
+                f.write(result)
+            else:
+                f.write(result.hex())
+        print(f"Hash written to {args.output}")
+    else:
+        if output_format == "hex":
+            print(f"Hash: {result}")
+        else:
+            print(f"Hash: {result.hex()}")
