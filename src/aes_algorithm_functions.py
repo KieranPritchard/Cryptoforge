7067d96a63678c686d2d1131b9d5815f969dac49
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-08-14 13:33:55 +0100

    added cfb mode

--- src/aes_algorithm_functions.py
+++ src/aes_algorithm_functions.py
@@ -6,18 +6,11 @@ from cryptography.hazmat.primitives.asymmetric import padding as asym_padding
 class AES:
     def __init__(self):
         pass
-
-    def padding(self, data):
+    
+    # CBC mode functions
+    def CBC_mode_plaintext_encryption(self, plaintext, key, iv):
         padder = sym_padding.PKCS7(128).padder()
-        padded_data = padder.update(data) + padder.finalize()
-        return padded_data
-    
-    def unpadder(self, data):
-        unpadder = sym_padding.PKCS7(128).unpadder()
-        plaintext = unpadder.update(data) + unpadder.finalize()
-        return plaintext.decode()
-    
-    def CBC_mode_plaintext_encryption(self, padded_plaintext, key, iv):
+        padded_plaintext = padder.update(plaintext) + padder.finalize()
         CBC_mode_cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
         CBC_mode_encryptor = CBC_mode_cipher.encryptor()
         ciphertext = CBC_mode_encryptor.update(padded_plaintext) + CBC_mode_encryptor.finalize()
@@ -27,7 +20,9 @@ class AES:
         CBC_mode_cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
         CBC_mode_decryptor = CBC_mode_cipher.decryptor()
         plaintext = CBC_mode_decryptor.update(ciphertext) + CBC_mode_decryptor.finalize()
-        return plaintext
+        unpadder = sym_padding.PKCS7(128).unpadder()
+        unpadded_plaintext = unpadder.update(plaintext) + unpadder.finalize()
+        return unpadded_plaintext
     
     def CBC_mode_file_encryption(self, file, key, iv):
         CBC_mode_cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
@@ -46,6 +41,43 @@ class AES:
         with open(f"{file}", "rb") as f:
             file_contents = f.read()
         decrypted_contents = CBC_mode_decryptor.update(file_contents) + CBC_mode_decryptor.finalize()
+        with open(f"{file}", "wb") as f:
+            f.write(decrypted_contents)
+
+    # CFB mode functions
+    def CFB_mode_plaintext_encryption(self, plaintext, key, iv):
+        padder = sym_padding.PKCS7(128).padder()
+        padded_plaintext = padder.update(plaintext) + padder.finalize()
+        CFB_mode_cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+        CFB_mode_encryptor = CFB_mode_cipher.encryptor()
+        ciphertext = CFB_mode_encryptor.update(padded_plaintext) + CFB_mode_encryptor.finalize()
+        return ciphertext
+    
+    def CFB_mode_ciphertext_decryption(self, ciphertext, key, iv):
+        CFB_mode_cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+        CFB_mode_decryptor = CFB_mode_cipher.decryptor()
+        plaintext = CFB_mode_decryptor.update(ciphertext) + CFB_mode_decryptor.finalize()
+        unpadder = sym_padding.PKCS7(128).unpadder()
+        unpadded_plaintext = unpadder.update(plaintext) + unpadder.finalize()
+        return unpadded_plaintext
+    
+    def CFB_mode_file_encryption(self, file, key, iv):
+        CFB_mode_cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+        CFB_mode_encryptor = CFB_mode_cipher.encryptor()
+
+        with open(f"{file}", "rb") as f:
+            file_contents = f.read()
+        encrypted_contents = CFB_mode_encryptor.update(file_contents) + CFB_mode_encryptor.finalize()
+        with open(f"{file}", "wb") as f:
+            f.write(encrypted_contents)
+
+    def CFB_mode_file_decryption(self, file, key, iv):
+        CFB_mode_cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+        CFB_mode_decryptor = CFB_mode_cipher.decryptor()
+
+        with open(f"{file}", "rb") as f:
+            file_contents = f.read()
+        decrypted_contents = CFB_mode_decryptor.update(file_contents) + CFB_mode_decryptor.finalize()
         with open(f"{file}", "wb") as f:
             f.write(decrypted_contents)
 
@@ -74,11 +106,8 @@ def handle_aes_operations(args, loaded_key):
         key_bytes = bytes.fromhex(key) if len(key) % 2 == 0 else key.encode()
         iv = bytes.fromhex(args.iv) if len(args.iv) % 2 == 0 else args.iv.encode()
         
-        # Pad data
-        padded_data = aes_cipher.padding(data)
-        
         # Encrypts the data
-        ciphertext = aes_cipher.CBC_mode_plaintext_encryption(padded_data, key_bytes, iv)
+        ciphertext = aes_cipher.CBC_mode_plaintext_encryption(data, key_bytes, iv)
         
         # Write output
         if args.plaintext:
@@ -108,19 +137,16 @@ def handle_aes_operations(args, loaded_key):
         # Decrypt
         plaintext = aes_cipher.CBC_mode_ciphertext_decryption(data, key_bytes, iv)
         
-        # Unpad
-        unpadded_data = aes_cipher.unpadder(plaintext)
-        
         # Write output
         if args.plaintext:
             if args.output:
                 with open(args.output, 'w') as f:
-                    f.write(unpadded_data)
+                    f.write(plaintext)
                 print(f"Decrypted data written to {args.output}")
             else:
-                print(f"Decrypted: {unpadded_data}")
+                print(f"Decrypted: {plaintext}")
         else:
             output_file = args.output if args.output else f"{args.input}.decrypted"
             with open(output_file, 'w') as f:
-                f.write(unpadded_data)
+                f.write(plaintext)
             print(f"Decrypted data written to {output_file}")
